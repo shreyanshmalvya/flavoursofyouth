ff3679bf07d714eb60014912813048807946b4f3
Shreyansh <shreyansh.malviya2020@vitbhopal.ac.in>
2022-08-19 21:58:36 +0530

    updated navlinks

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -1,21 +1,27 @@
 import React from 'react'
 import logo from '../../assets/images/logo.svg'
 import './navbar.css'
-
+import { Link } from 'react-router-dom'
 
 const Navbar = () => {
   return (
     <div className="navbarWrapper">
       <div className="logo">
-        <img src={logo} alt="logo" />
+        <Link to="/" style={{ textDecoration: 'none', color: 'black' }}>
+          <img src={logo} alt="logo" />
+        </Link>
       </div>
       <div className="joinWrapper">
-        <div className="joinButton createButton">
-          <span className='create'>Create Room</span>
-        </div>
-        <div id="join" className="joinButton">
-          <span className='join'>Join Room</span>
-        </div>
+        <Link to="/create-room" style={{ textDecoration: 'none', color: 'black' }}>
+          <div className="joinButton createButton">
+            <span className='create'>Create Room</span>
+          </div>
+        </Link>
+        <Link to="/join-room" style={{ textDecoration: 'none', color: 'black' }}>
+          <div id="join" className="joinButton">
+            <span className='join'>Join Room</span>
+          </div>
+        </Link>
       </div>
     </div>
   )
