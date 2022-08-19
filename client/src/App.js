5184edf702583ce75a5741a388157d0459fd7d55
Shreyansh <shreyansh.malviya2020@vitbhopal.ac.in>
2022-08-19 21:59:48 +0530

    added routes

--- client/src/App.js
+++ client/src/App.js
@@ -2,13 +2,25 @@ import './App.css';
 import io from 'socket.io-client'
 import React from 'react';
 import Home from './pages/home/Home';
+import CreateRoomPage from './pages/createRoom/CreateRoomPage';
+import JoinPage from './pages/joinRoom/JoinPage';
+import Chatpage from './pages/chat/Chatpage';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
-
+//estblishing connection with web socket
+const socket = io.connect('http://localhost:8080');
 
 function App() {
   return (
     <div className="App">
-    <Home />
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home socket={socket} />} />
+          <Route path="/create-room" element={<CreateRoomPage socket={socket} />} />
+          <Route path="/join-room" element={<JoinPage socket={socket} />} />
+          <Route path="/chat" element={<Chatpage socket={socket} />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
