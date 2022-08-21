b9ab53bf2dd2d61dbb69808bc90da310cc1df817
Shreyansh <shreyansh.malviya2020@vitbhopal.ac.in>
2022-08-22 02:29:52 +0530

    implimented multi directional messaging

--- client/src/components/chats/Chat.jsx
+++ client/src/components/chats/Chat.jsx
@@ -1,12 +1,15 @@
 import React, { useEffect } from 'react'
 import { useSelector } from 'react-redux';
+import './chat.css'
+import ScrollToBottom from 'react-scroll-to-bottom';
 
-function Chat({ socket, username }) {
+function Chat({ socket }) {
   const [currentMessage, setCurrentMessage] = React.useState('');
   const [messageList, setMessageList] = React.useState([]);
 
   const roomName = useSelector(state => state.roomData.roomData.name);
   const roomID = (useSelector(state => state.roomData.roomData.id));
+  const user = useSelector(state => state.username.username);
 
 
   const sendMessage = async () => {
@@ -14,7 +17,7 @@ function Chat({ socket, username }) {
       const messageData = {
         room: roomID,
         topic: roomName,
-        author: username,
+        author: user,
         message: currentMessage,
         time: new Date(Date.now()).getHours() + ":" + new Date(Date.now()).getMinutes()
       }
@@ -36,21 +39,46 @@ function Chat({ socket, username }) {
   }, [socket])
 
   return (
-    <>
-      <div className="chatWrapper">
-        {messageList.map((message, key) => {
-          return (
-            <div className="chatMessage" key = {key}>
-              <div className="chatAuthor">{message.author}</div>
-              <div className="chatMessageText">{message.message}</div>
-              <div className="chatTime">{message.time}</div>
+    <div className='chat'>
+      <div className="wrap">
+        <div className="chatHeader">
+          <div className="chatHeaderText">
+             <span>Topic :</span>  {roomName}
+          </div>
+        </div>
+        <div className="chatWrapper">
+          <ScrollToBottom className='messageContainer'>
+          {messageList.map((message, key) => {
+            return (
+              <div className={user === message.author ? "you " : "other"} key={key}>
+                <div className="chatMessage">
+                  <div className="chatMessageText">{message.message}</div>
+                  <div className="messageAlignWrapper">
+                    <div className="chatAuthor">{message.author},</div>
+                    <div className="chatTime">{message.time}</div>
+                  </div>
+                </div>
+              </div>
+            )
+          })}
+          </ScrollToBottom>
+        </div>
+        <div className="chatFooter">
+          <div className="searchWrapper">
+            <div className="search">
+              <div className="searchInput">
+                <input type="text" value={currentMessage} onChange={(e) => setCurrentMessage(e.target.value)} onKeyPress={(event) => {
+                  event.key === 'Enter' && sendMessage();
+                }} />
+              </div>
+              <div className="searchIcon chatfooter" onClick={sendMessage}>
+                SEND
+              </div>
             </div>
-          )
-        })}
+          </div >
+        </div>
       </div>
-      <input type="text" value={currentMessage} onChange={(e) => setCurrentMessage(e.target.value)} />
-      <button onClick={sendMessage}>Send</button>
-    </>
+    </div>
   )
 }
 
