4a2ac2e45f22526f27c0ee8d75fbc0aed543c41a
Shreyansh <shreyansh.malviya2020@vitbhopal.ac.in>
2022-08-21 23:20:01 +0530

    added new route variable to fix routing error

--- server/index.js
+++ server/index.js
@@ -31,79 +31,84 @@ io.on("connection", (socket) => {
     //output for searching rooms
     socket.on("search_room", (data) => {
         console.log(data);
-        Room.find({ name: data}).exec()
-        .then(result => {
-            console.log(result)
-            if (result.length > 0) {
-                socket.emit("room_data", {
-                    room: result
-                });
-            } else {
-                socket.emit("room_data", {
-                    room: null
-                });
-            }
-        })
-        .catch(err =>{
-            console.log(err);
-        })
+        Room.find({ name: data }).exec()
+            .then(result => {
+                console.log(result)
+                if (result.length > 0) {
+                    socket.emit("room_data", {
+                        room: result
+                    });
+                } else {
+                    socket.emit("room_data", {
+                        room: null
+                    });
+                }
+            })
+            .catch(err => {
+                console.log(err);
+            })
     })
 
     //output for creating rooms
     socket.on("create_room", (data) => {
         console.log(data);
         const room = new Room({
-            _id : new mongoose.Types.ObjectId(),
-            name : data.roomName,
+            _id: new mongoose.Types.ObjectId(),
+            name: data.roomName,
             description: data.roomDescription,
         });
         room.save()
-        .then(result => {
-            socket.join(result._id);
-            socket.emit("room_created", {
-                message : true,
-                result : result
-            })
-            console.log((result._id));
-        }).catch(err => {
-            console.log(err);
-            socket.emit("room_created", {
-                message : false
-            })
-        }
-        )
+            .then(result => {
+                const room = result._id.toHexString();
+                socket.join(room);
+                console.log(socket.rooms);
+                
+                socket.emit("room_created", {
+                    message: true,
+                    result: result
+                })
+                console.log((result._id));
+            }).catch(err => {
+                console.log(err);
+                socket.emit("room_created", {
+                    message: false
+                })
+            }
+            )
     });
-    
+
     //output for joining rooms
     socket.on("join_room_id", (data) => {
         console.log(data);
         Room.findById(data).exec()
-        .then(result => {
-            console.log(result);
-            if (result) {
-                socket.join(result._id);
-                socket.emit("room_info", {
-                    message: "joined room",
-                    result: result,
-                });
-            }else{
-                socket.emit("room_info", {
-                    message: "room not found",
-                });
-            }
-        }).catch(err => {
-            console.log(err);
-        })
+            .then(result => {
+                if (result) {
+                    const room = result._id.toHexString();
+                    socket.join(room);
+                    console.log(socket.rooms);
+                    console.log(`user ${socket.id} joined room ${room}`);
+                    socket.emit("room_info", {
+                        message: "joined room",
+                        result: result,
+                    });
+                } else {
+                    socket.emit("room_info", {
+                        message: "room not found",
+                    });
+                }
+            }).catch(err => {
+                console.log(err);
+            })
     });
 
     //brodcasting messages
     socket.on("send_message", (data) => {
-        console.log(data);
+        console.log(`user with id ${socket.id} sent message ${data.message} to room new ObjectId("${data.room}")`);
         //emitting recieved message to all clients in the room
-        socket.to(data.roomId).emit("receive_message", data)
+        socket.to(`${data.room}`).emit("recieved_message", data);
     })
 
-    socket.on("disconnect", (data) => {
+    socket.on("disconnect", () => {
         console.log(`user ${socket.id} disconnected`);
         // console.log(socket.rooms.get(data.roomId).size);
     });
