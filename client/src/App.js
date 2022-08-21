7cde1fbaa1a2e044bdd0c5b1236a577b9a16c165
Shreyansh <shreyansh.malviya2020@vitbhopal.ac.in>
2022-08-21 23:18:13 +0530

    modified socket

--- client/src/App.js
+++ client/src/App.js
@@ -6,16 +6,11 @@ import CreateRoomPage from './pages/createRoom/CreateRoomPage';
 import JoinPage from './pages/joinRoom/JoinPage';
 import Chatpage from './pages/chat/Chatpage';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import {useSelector} from 'react-redux';
 
 //estblishing connection with web socket
 const socket = io.connect('http://localhost:8080');
 
 function App() {
-  const roomName = useSelector(state => state.roomData.roomName);
-  const roomID = useSelector(state => state.roomData.roomID);
-  console.table(roomName, roomID);
-
   return (
     <div className="App">
       <Router>
@@ -23,7 +18,7 @@ function App() {
           <Route path="/" element={<Home socket={socket} />} />
           <Route path="/create-room" element={<CreateRoomPage socket={socket} />} />
           <Route path="/join-room" element={<JoinPage socket={socket} />} />
-          <Route path="/chat" element={<Chatpage socket={socket} roomName={roomName} roomID = {roomID}/>} />
+          <Route path="/chat" element={<Chatpage socket={socket} />} />
         </Routes>
       </Router>
     </div>
